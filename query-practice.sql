e7f9e5c238a3e67ec07618f361f1083221c1985a
kopalgarg <kopalgarg24@gmail.com>
2021-05-13 19:16:38 -0600

    Update query-practice.sql

--- query-practice.sql
+++ query-practice.sql
@@ -39,6 +39,50 @@ WHERE state NOT IN ('VA', 'GA', 'FL');
 SELECT *
 FROM products
 WHERE quantity_in_stock IN (49, 38, 72);
+# get customers with points >= 1000 <= 3000
+SELECT *
+FROM customers
+WHERE points BETWEEN 1000 AND 3000;
+# return custmers born between 1/1/1990 and 1/1/2000
+SELECT *
+FROM customers
+WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01';
+# last name with B somewhere and first name with I
+SELECT *
+FROM customers 
+WHERE last_name LIKE '%b%' AND first_name LIKE 'i%';
+# first name exactly 4 characters long and last letter is y
+SELECT * FROM customers 
+WHERE first_name LIKE '___y';
+-- get customers with address containing 
+-- trail or avenue and phone number not ending with 9
+SELECT * FROM customers
+where (address LIKE '%trail%' OR 
+	   address LIKE '%avenue%') AND 
+       phone NOT LIKE '%9';
+-- the last one with regular expressions
+select * from customers
+where (address regexp 'trail' or 
+	   address regexp 'avenue') and 
+       phone not like '%9';
+# last name must end with field and first name starts with I
+select * from customers where (last_name regexp 'field$' and first_name regexp '^I');
+# last names with field as suffix or mac as prefix in last name 
+select * from customers where last_name regexp 'field$|^mac';
+# last names with letter e but before e we have any letter from a to  i
+select * from customers where last_name regexp '[a-i]e' 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
