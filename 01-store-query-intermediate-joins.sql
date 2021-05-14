2577f5e680e8a0333fe0a6f4e444958e4062a381
kopalgarg <kopalgarg24@gmail.com>
2021-05-14 10:56:32 -0600

    Update 01-store-query-intermediate-joins.sql

--- 01-store-query-intermediate-joins.sql
+++ 01-store-query-intermediate-joins.sql
@@ -1,20 +1,11 @@
 /*
-Null
-order by
 Inner joins
 Full/right/left outer joins
 */
--- phone number is null
-select * from customers where phone is null;
--- orders not shipped yet
-select * from orders where shipped_date is null;
--- order by (ascending)
-select * from orders order by order_id asc;
--- order by two col
-select first_name, last_name, points/10 AS disc from customers order by state desc, points desc;
-select *, unit_price * quantity as total_price from order_items where order_id = 2 order by total_price desc
-
-
-
+-- inner join and alias for names 
+select order_id, first_name, o.customer_id, last_name from orders o inner join customers c
+							on o.customer_id = c.customer_id;
+select oi.order_id, oi.product_id, oi.quantity, oi.unit_price from order_items oi inner join products p on oi.product_id = p.product_id
+-- combine columns across multiple db
 
 
