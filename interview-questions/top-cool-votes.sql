8036a6ccef9b72391c7d617ede63522afd621437
kopalgarg <kopalgarg24@gmail.com>
2021-05-14 20:36:36 -0600

    Create top-cool-votes.sql

---
+++ interview-questions/top-cool-votes.sql
@@ -0,0 +1,7 @@
+/*
+Top Cool Votes
+Link: https://platform.stratascratch.com/coding-question?id=10060&python=
+*/
+select business_name, review_text 
+from yelp_reviews
+where cool = (select max(cool) from yelp_reviews)
