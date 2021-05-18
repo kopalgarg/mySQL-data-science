c1530595ca2fc84806dc1c3992ae24e794896263
kopalgarg <kopalgarg24@gmail.com>
2021-05-17 21:47:02 -0600

    add another sol

--- interview-questions/second-highest-salary.sql
+++ interview-questions/second-highest-salary.sql
@@ -2,7 +2,12 @@
 176. Second Highest Salary
 Link: https://leetcode.com/problems/second-highest-salary/
 */
+-- solution 1
 select distinct e1.Salary as SecondHighestSalary from Employee e1
 where 1 = (select count(distinct e2.Salary) 
              from Employee e2
-             where e2.Salary > e1.Salary)
+             where e2.Salary > e1.Salary);
+             
+-- solution 2
+select max(e1.sal) from Employee e1 
+where e1.sal not in (select max(e2.Salary) from Employee e2);
