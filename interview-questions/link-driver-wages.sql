6b77922080be85a32bf98ebe4420c325180ebb1c
kopalgarg <kopalgarg24@gmail.com>
2021-05-20 17:11:20 -0600

    Create link-driver-wages.sql

---
+++ interview-questions/link-driver-wages.sql
@@ -0,0 +1,6 @@
+/*
+Lyft Driver Wages
+Link: https://platform.stratascratch.com/coding-question?id=10003&python=
+*/
+select * from lyft_drivers
+where yearly_salary <= 30000 or yearly_salary >=70000
