93ce30da8cb45c9d420d31c3f25826ad6e73297e
kopalgarg <kopalgarg24@gmail.com>
2021-05-22 11:00:57 -0600

    Update project-employee-count.sql

--- project-employee-count.sql
+++ project-employee-count.sql
@@ -2,8 +2,15 @@
 Project Employees II
 LeetCode
 */
+-- solution1 
 select distinct project_id
 from Project
 group by project_id
 having count(employee_id) = (select count(employee_id) as count from Project
 							group by project_id order by count desc limit 1)
+                            
+-- solution2
+with sub as (select project_id, count(*) as count from Project group by project_id)
+
+select project_id from sub
+where count = (select max(cnt) from sub)
