212c33b29b2c1452996020ace30082f90f7fa1ca
kopalgarg <kopalgarg24@gmail.com>
2021-05-22 10:54:10 -0600

    Create duplicate-email.sql

---
+++ duplicate-email.sql
@@ -0,0 +1,10 @@
+/*
+Duplicate Emails
+Link: https://leetcode.com/problems/duplicate-emails/
+*/
+
+# Write your MySQL query statement below
+select email 
+from Person 
+group by email 
+having count(email)>1
